976c47b3d0e1eaa6042b42780e6aa195f6199cf9
shenlu89 <shenlu89dev@gmail.com>
2025-10-23 14:58:01 +0800

    refactor(layout): update fonts and add theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit, Roboto_Mono } from 'next/font/google'
+import { ThemeProvider } from 'next-themes'
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const outfit = Outfit({
+  variable: '--font-outfit',
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const robotoMono = Roboto_Mono({
+  variable: '--font-roboto-mono',
   subsets: ["latin"],
 });
 
@@ -23,11 +24,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en"
+      suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${outfit.variable} ${robotoMono.variable} font-sans antialiased h-screen`}
       >
-        {children}
+        <ThemeProvider attribute="class">
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
