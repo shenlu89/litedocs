b08ed96c0ac2d0ba52de72fab04c52021fa26e74
shenlu89 <shenlu89dev@gmail.com>
2025-10-25 01:24:34 +0800

    refactor(logo): simplify svg props and remove cn utility

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -14,11 +14,12 @@ export const Logo: React.FC<LogoProps> = ({
   return (
     <svg
       version="1.1"
-      width={size}
-      height={size} viewBox="275 125 500 750"
-      fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      className={cn("select-none transition-colors duration-300 ease-in-out", className)}
+      width={size}
+      height={size}
+      viewBox="275 125 500 750"
+      fill="none"
+      className={className}
       role="img"
       aria-label="Logo"
       {...props}
