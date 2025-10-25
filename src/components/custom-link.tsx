6938139a75e1aa0d9faa700dc5c0c8d4dc24d3cb
shenlu89 <shenlu89dev@gmail.com>
2025-10-26 02:39:18 +0800

    feat(components): add custom-link component for enhanced link handling

---
+++ src/components/custom-link.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+const CustomLink = (props: any) => {
+  const { href } = props;
+  if (href.startsWith("#")) {
+    return <a {...props} />;
+  }
+  if (!href.startsWith("/")) {
+    props = {
+      target: "_blank",
+      rel: "noopener noreferrer",
+      prefetch: false,
+      ...props,
+    };
+  }
+
+  return <Link {...props} />;
+};
+
+export default CustomLink;
