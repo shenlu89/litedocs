62b5145b30f94fa90183dc55c2dd38699c675b3f
shenlu89 <shenlu89dev@gmail.com>
2025-10-29 15:40:24 +0800

    build(content): enhance mdx processing with rehype plugins

--- content-collections.ts
+++ content-collections.ts
@@ -1,16 +1,73 @@
 import { defineCollection, defineConfig } from "@content-collections/core";
+import { compileMDX } from "@content-collections/mdx";
+import rehypeShiki from "@shikijs/rehype";
+import {
+  transformerMetaHighlight,
+  transformerMetaWordHighlight,
+  transformerNotationDiff,
+  transformerNotationFocus,
+  transformerNotationHighlight,
+  transformerNotationWordHighlight,
+} from "@shikijs/transformers";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
 import { z } from "zod";
 
-const posts = defineCollection({
-  name: "posts",
-  directory: "src/posts",
-  include: "**/*.md",
+const pages = defineCollection({
+  name: "pages",
+  directory: "src/content",
+  include: "**/*.mdx",
   schema: z.object({
     title: z.string(),
-    summary: z.string(),
+    description: z.string(),
   }),
+  transform: async (document, context) => {
+    // Compile MDX with rehype plugins
+    const body = await compileMDX(context, document, {
+      rehypePlugins: [
+        rehypeSlug,
+        [
+          rehypeShiki,
+          {
+            transformers: [
+              transformerNotationDiff(),
+              transformerNotationFocus(),
+              transformerNotationHighlight(),
+              transformerNotationWordHighlight(),
+              transformerMetaHighlight(),
+              transformerMetaWordHighlight(),
+            ],
+            theme: "github-dark-default",
+          },
+        ],
+        [
+          rehypeAutolinkHeadings,
+          {
+            properties: {
+              className: ["subheading-anchor"],
+              ariaLabel: "Link to section",
+            },
+          },
+        ],
+      ],
+    });
+
+    const id = document._meta.fileName.replace(".mdx", "");
+
+    const slug = document._meta.path;
+
+    const category = document._meta.directory;
+
+    return {
+      id,
+      ...document,
+      slug,
+      category,
+      body,
+    };
+  },
 });
 
 export default defineConfig({
-  collections: [posts],
+  collections: [pages],
 });
