41eae3633dd87d4d6bbd171b503e936acf4f580d
shenlu89 <shenlu89dev@gmail.com>
2025-10-31 12:42:20 +0800

    fix: update discord invite link to new server

--- src/components/discord-button.tsx
+++ src/components/discord-button.tsx
@@ -4,7 +4,7 @@ import Link from "@/components/custom-link";
 export const DiscordButton: React.FC = () => {
   return (
     <Link
-      href="https://discord.gg/aeNbhwng4E"
+      href="https://discord.gg/c2mKdBVt7n"
       className="text-gray-600 dark:text-gray-300 hover:text-black dark:hover:text-gray-200"
     >
       <FaDiscord className="size-6" />
