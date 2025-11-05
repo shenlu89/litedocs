6cbf0f8a63b6b9bf6ec5c8dc412192e73924896b
shenlu89 <shenlu89dev@gmail.com>
2025-11-06 02:08:20 +0800

    style: improve spacing and font weight styling

--- src/components/table-of-contents.tsx
+++ src/components/table-of-contents.tsx
@@ -98,16 +98,16 @@ interface TreeProps {
 
 function Tree({ tree, level = 1, activeItem }: TreeProps) {
   return tree?.items?.length && level < 3 ? (
-    <ul className={cn("m-0 list-none", { "pl-4": level !== 1 })}>
+    <ul className={cn("m-0 list-none space-y-2", { "pl-4": level !== 1 })}>
       {tree.items.map((item, index) => {
         return (
-          <li key={index} className={cn("mt-0 pt-2")}>
+          <li key={index} className={cn("mt-0 space-y-2")}>
             <a
               href={item.url}
               className={cn(
                 "inline-block no-underline hover:text-foreground",
                 item.url === `#${activeItem}`
-                  ? "font-semibold text-foreground"
+                  ? "text-foreground"
                   : "text-muted-foreground"
               )}
             >
