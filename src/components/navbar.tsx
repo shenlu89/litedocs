00d45475fa328c7f91ffd96737f52f41fd226316
shenlu89 <shenlu89dev@gmail.com>
2025-12-03 21:40:39 +0800

    refactor(navbar): consolidate social buttons

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,10 +2,22 @@
 
 import { type HTMLAttributes } from "react";
 import { cn } from "@/lib/utils";
-
-import { DiscordButton } from "@/components/discord-button";
-import { GithubButton } from "@/components/github-button";
+import Link from "@/components/custom-link";
+import { FaDiscord, FaGithub } from "react-icons/fa6";
 import ThemeButton from "@/components/theme-button";
+
+const socialIcons = [
+  {
+    title: "Discord",
+    href: "https://discord.gg/c2mKdBVt7n",
+    icon: FaDiscord,
+  },
+  {
+    title: "GitHub",
+    href: "https://github.com/shenlu89/litedocs",
+    icon: FaGithub,
+  },
+];
 
 interface Navbar extends HTMLAttributes<HTMLDivElement> { }
 
@@ -25,9 +37,22 @@ export const AppNavbar: React.FC<Navbar> = ({
       {...props}
     >
       <nav className="flex items-center gap-4">
-        <ThemeButton />
-        <DiscordButton />
-        <GithubButton />
+        <ul className="flex justify-center items-center space-x-4">
+          <li>
+            <ThemeButton />
+          </li>
+          {socialIcons.map((social) => (
+            <li key={social.title}>
+              <Link
+                className="text-slate-600 dark:text-slate-300 hover:text-black dark:hover:text-slate-200"
+                title={social.title}
+                href={social.href}
+              >
+                <social.icon className="size-6" />
+              </Link>
+            </li>
+          ))}
+        </ul>
       </nav>
     </header>
   );
